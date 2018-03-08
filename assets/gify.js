36f34fc4c0613c3085dae70bc83c6a9c2cd8f7ea
Amanda Ogonosky <amandaogonosky@Os-MacBook-Air.local>
2018-03-07 20:52:12 -0700

    hw

---
+++ assets/gify.js
@@ -0,0 +1,32 @@
+console.log("workin")
+
+var gifButton;
+var queryURL;
+var topics;
+
+// function
+//  gify params: q, limit, rating
+// for (var i =0; i< arr.length -1; i++){
+//     for (var j=0; j<arr.lenghty-1; j++){
+//    if (arr[j] >arr[j+1];
+//    arr[j+1] =arr[j];
+//    arr[j] =temp;
+//    }
+//    }
+// //    ]
+   
+// event
+// cyU2OBRcwA8cePhq5korZnRYv2sFFzqx
+
+// $.ajax()({
+//     url =queryURL,
+//     method: 'GET',
+
+
+// })
+
+
+$("#giftop").click(function(){
+    // $("#giftop").console
+console.log(gifButton);
+});
