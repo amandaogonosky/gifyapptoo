13b741a8bd034018a1469a9f395629374414d527
Amanda Ogonosky <amandaogonosky@Os-MacBook-Air.local>
2018-03-18 19:22:26 -0600

    working

--- assets/gify.js
+++ assets/gify.js
@@ -3,47 +3,85 @@ console.log("workin")
 var gifButton;
 var queryURL;
 var topics;
-
+var giffy = ["cat", "kitten","rainbow"];
 // function
 //  gify params: q, limit, rating
 
-var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=cats&api_key=cyU2OBRcwA8cePhq5korZnRYv2sFFzqx&limit=10");
-xhr.done(function(data) { console.log("success got data", data); });
+// var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=cats&api_key=cyU2OBRcwA8cePhq5korZnRYv2sFFzqx&limit=10");
+// xhr.done(function(data) { console.log("success got data", data); });
 				
-var giffy = ["cat", "kitten","rainbow"];
+
   function renderButtons() {
-  $("#find-gif").empty();
+  $("#giffy-button").empty();
 
-for (var i =0; i< giffy.lenght; i++){
- var a = $("<button>");
- a.addClass("giffy");
- a.attr("data-name",giffy[i]);
- a.text(gifButton[i]);
+for (var i =0; i< giffy.length; i++){
+ var show = $("<button>");
+ show.addClass("giffy");
+ show.attr("data-name",giffy[i]);
+ show.text(giffy[i]);
 
- $("#giffy-button").append(a);
+ $("#giffy-button").append(show);
 
     } 
   }
-$("#giftop").on("click",function(event){
-event.preventDefault();
+  renderButtons();
 
-var giffy = $("#gif-form").val().trim();
-giffy.push(giffy);
 
+$("#find-gif").on("click", function(event){
+  event.preventDefault();
+  var getGif =$("#gif-input").val().trim();
+  giffy.push(getGif)
+  renderButtons();
+  $("#gif-input").val(" ");
+console.log(getGif);
 });
 
-renderButtons();
+$(document).on("click", ".giffy", displaygif)
+// }
 // });
 
-// $.ajax()({
-//     url =queryURL,
-//     method: 'GET',
+function displaygif(){
+  var queryURL ="https://api.giphy.com/v1/gifs/search?q="+($(this).text())+"&api_key=cyU2OBRcwA8cePhq5korZnRYv2sFFzqx&limit=10";
+  
+  $.ajax({
+    url: queryURL,
+    method: 'GET',
+}).then(function(response){
+console.log(response);
+for (i=0; i <response.data.length; i++){
+  var gifMove =response.data[i].images.fixed_height.url;
+  var gifStill =response.data[i].images.fixed_height_still.url;
+  var gifs =$("<img>").addClass("anyGif")
+  .attr("src", gifStill).attr("gifMove", gifMove).attr("gifStill", gifStill).attr("data-state", "still")
+  $(".gif-views").append(gifs);
+
+}
+
+});
+$(document).on("click", ".anyGif",function()
+
+{
+  var state = $(this).attr("data-state");
+      if (state==="still") {
+$(this).attr("src", $(this).attr("gifMove"));
+        $(this).attr("data-state", "moving")
+        console.log("clicked");
+}
+    else{
+      $(this).attr("src", $(this).attr("gifStill"));
+      $(this).attr("data-state", "stilling")
+
+    }
+})
+}
+
+// var gifDiv = $("<div class='gif'>");
+
+// var gifThing = response.img
 
 
-// })
 
 
-// $("#giftop").click(function(){
-//     // $("#giftop").console
-// console.log(gifButton);
-// });
+
+
+
